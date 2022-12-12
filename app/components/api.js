b325f9695f98dcd4e6362cc9a0e9e707e1e2dea4
ishan-singh-3005 <ishan423@gmail.com>
2022-12-12 15:11:32 -0500

    fixed error

--- app/components/api.js
+++ app/components/api.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const completion = await openai.createCompletion({
     model: "text-davinci-002",
-    prompt: generatePrompt(req.body.animal),
+    prompt: generatePrompt(req.body.question),
     temperature: 0.6,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
